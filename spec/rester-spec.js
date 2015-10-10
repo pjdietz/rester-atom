8e138d0df402d7189f3e07128d4d8186da9239b6
PJ Dietz <pjdietz@gmail.com>
2015-10-10 18:34:39 -0400

    Test for modal view activating and displaying

--- spec/rester-spec.js
+++ spec/rester-spec.js
@@ -6,36 +6,33 @@ var rester = require("../lib/rester");
 
 describe("RESTer", function () {
 
-    var workspaceElement;
+    var activationPromise, getModalPanel, workspaceElement;
 
     beforeEach(function() {
+        activationPromise = atom.packages.activatePackage("rester");
         workspaceElement = atom.views.getView(atom.workspace);
-        waitsForPromise(function() {
-            return atom.packages.activatePackage("rester");
-        });
     });
-    
+
     it("displays modal view when starting request", function () {
-
-        var panels, filter;
-
-        filter = function (panel) {
-            var classes = panel.getItem().classList, i, u;
-            for (i = 0, u = classes.length; i < u; ++i) {
-                if (classes[i] === "rester-request-progress") {
-                    return true;
-                }
-            }
-            return false;
-        };
-
-        panels = atom.workspace.getModalPanels().filter(filter);
-        expect(panels.length).toEqual(0);
-
-        atom.commands.dispatch(workspaceElement, "rester:request");
-
-        panels = atom.workspace.getModalPanels().filter(filter);
-        expect(panels.length).toEqual(1);
+        var panel;
+        runs(function () {
+            expect(workspaceElement.querySelector(".rester-request-progress")).not.toExist();
+            atom.commands.dispatch(workspaceElement, "rester:request");
+        });
+        waitsForPromise(function () {
+            return activationPromise;
+        });
+        runs(function () {
+            expect(workspaceElement.querySelector(".rester-request-progress")).toExist();
+        });
+        waitsFor(function () {
+            var item = workspaceElement.querySelector(".rester-request-progress");
+            panel = atom.workspace.panelForItem(item);
+            return panel !== undefined;
+        });
+        runs(function () {
+            expect(panel.isVisible()).toBe(true);
+        });
 
     });
 
