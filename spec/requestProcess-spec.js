4de314177b1ca2325abe51e94084fb14984badd4
PJ Dietz <pjdietz@gmail.com>
2016-03-09 20:50:54 -0500

    Update specs to ES6

--- spec/requestProcess-spec.js
+++ spec/requestProcess-spec.js
@@ -1,22 +1,23 @@
-var RequestProcess = require("../lib/requestProcess").RequestProcess;
+'use strict';
 
-describe("RequestProcess", function () {
+const RequestProcess = require('../lib/requestProcess').RequestProcess;
 
-    describe("Selection", function () {
+describe('RequestProcess', function () {
 
-        var lines = [
-            "POST http://localhost/path HTTP/1.1",
-            "Host: localhost",
-            "Content-type: text/plain",
-            "",
-            "This is the body",
-            ""],
-            text = lines.join("\n");
+    describe('Selection', function () {
 
-        beforeEach(function() {
-            var editor;
-            waitsForPromise(function() {
-                var promise = atom.workspace.open();
+        const text = `
+            POST http://localhost/path HTTP/1.1
+            Host: localhost
+            Content-type: text/plain
+
+            This is the body
+            `.trim();
+        const lines = text.split('\n');
+
+        beforeEach(() => {
+            waitsForPromise(() => {
+                const promise = atom.workspace.open();
                 promise.then(function (editor) {
                     editor.setText(text);
                 });
@@ -24,39 +25,35 @@ describe("RequestProcess", function () {
             });
         });
 
-        it("includes entire buffer when no text is selected", function () {
-            var editor = atom.workspace.getActiveTextEditor(),
-                proc = new RequestProcess(editor),
-                selection = proc.getSelectedText(editor);
+        it('Includes entire buffer when no text is selected', () => {
+            const editor = atom.workspace.getActiveTextEditor();
+            const proc = new RequestProcess(editor);
+            const selection = proc.getSelectedText(editor);
             editor.setText(text);
             expect(selection).toEqual(text);
         });
 
-        it("includes only selected lines when text is selected", function () {
-            var editor = atom.workspace.getActiveTextEditor(),
-                proc = new RequestProcess(editor),
-                selection;
+        it('Includes only selected lines when text is selected', () => {
+            const editor = atom.workspace.getActiveTextEditor();
+            const proc = new RequestProcess(editor);
             editor.setCursorBufferPosition([0, 0]);
             editor.selectDown(2);
-            selection = proc.getSelectedText(editor);
-            expect(selection).toContain(lines.slice(0,2).join("\n"));
-            expect(selection).not.toContain(lines.slice(3).join("\n"));
+            const selection = proc.getSelectedText(editor);
+            expect(selection).toContain(lines.slice(0,2).join('\n'));
+            expect(selection).not.toContain(lines.slice(3).join('\n'));
         });
 
-        it("includes multiple selections concatenated", function () {
-            var editor = atom.workspace.getActiveTextEditor(),
-                proc = new RequestProcess(editor),
-                selection;
+        it('includes multiple selections concatenated', function () {
+            const editor = atom.workspace.getActiveTextEditor();
+            const proc = new RequestProcess(editor);
             editor.setCursorBufferPosition([0, 0]);
             editor.addCursorAtBufferPosition([4, 0]);
             editor.selectToEndOfLine();
-            selection = proc.getSelectedText(editor);
+            const selection = proc.getSelectedText(editor);
             expect(selection).toContain(lines[0]);
             expect(selection).not.toContain(lines[1]);
             expect(selection).not.toContain(lines[2]);
             expect(selection).toContain(lines[4]);
         });
-
     });
-
 });
--- spec/rester-spec.js
+++ spec/rester-spec.js
@@ -1,188 +1,188 @@
-/* jshint node: true, jasmine: true */
-/* globals atom, waitsForPromise */
-"use strict";
+'use strict';
 
-var http = require("http");
+const http = require('http');
+const rester = require('../lib/rester');
 
-var rester = require("../lib/rester");
+/** Port to use for local servers. */
+const port = 8123;
+/** Text to use to make a request to the local server. */
+const requestTest = `GET http://localhost:${port}/`;
 
-rester.createRequestProcess = function () {
-    console.log("Hello, override!");
-};
+// -----------------------------------------------------------------------------
 
-describe("RESTer", function () {
+describe('RESTer', function () {
 
-    var activationPromise, workspaceElement,
-        /** Port to use for local servers. */
-        port = 8123,
-        /** Text to use to make a request to the local server. */
-        requestTest = "GET http://localhost:8123/";
+    let activationPromise;
+    let workspaceElement;
 
     beforeEach(function() {
-        activationPromise = atom.packages.activatePackage("rester");
+        activationPromise = atom.packages.activatePackage('rester');
         workspaceElement = atom.views.getView(atom.workspace);
     });
 
-    it("displays modal view when starting request", function () {
-        var panel, server = http.createServer();
-        // Start a local server that starts but never finished a response.
+    it('Displays modal view when starting request', function () {
+        let server;
+        // Start a local server that starts but never finishes a response.
         // This gives the client time to test for the modal panel.
-        runs(function () {
-            server.on("request", function(request, response) {
+        runs(() => {
+            server = http.createServer();
+            server.on("request", (request, response) => {
                 response.statusCode = 200;
                 response.write("Hello, world!");
             });
             server.listen(port);
         });
         // Open an editor and set the text.
-        waitsForPromise(function () {
-            var promise = atom.workspace.open();
-            promise.then(function (editor) {
+        waitsForPromise(() => {
+            const promise = atom.workspace.open();
+            promise.then((editor) => {
                 editor.setText(requestTest);
             });
             return promise;
         });
         // Run the command.
-        runs(function () {
-            expect(workspaceElement.querySelector(".rester-request-progress")).not.toExist();
-            atom.commands.dispatch(workspaceElement, "rester:request");
+        runs(() => {
+            expect(workspaceElement.querySelector('.rester-request-progress')).not.toExist();
+            atom.commands.dispatch(workspaceElement, 'rester:request');
         });
         // Wait until the package is activated.
-        waitsForPromise(function () {
+        waitsForPromise(() => {
             return activationPromise;
         });
         // The view element should exist.
-        runs(function () {
-            expect(workspaceElement.querySelector(".rester-request-progress")).toExist();
+        runs(() => {
+            expect(workspaceElement.querySelector('.rester-request-progress')).toExist();
         });
         // Wait for a modal panel.
-        waitsFor(function () {
-            var item = workspaceElement.querySelector(".rester-request-progress");
+        let panel;
+        waitsFor(() => {
+            const item = workspaceElement.querySelector('.rester-request-progress');
             panel = atom.workspace.panelForItem(item);
             return panel !== undefined;
         });
         // The modal panel should be visible.
-        runs(function () {
+        runs(() => {
             expect(panel.isVisible()).toBe(true);
             server.close();
         });
     });
 
-    describe("Errors", function () {
+    describe('Errors', () => {
         // These tests will not set up a server; this will cause a Connection
         // Refused error.
-        it("displays error alert", function () {
-            waitsForPromise(function () {
-                var promise = atom.workspace.open();
-                promise.then(function (editor) {
+        it('Displays error alert', () => {
+            waitsForPromise(() => {
+                const promise = atom.workspace.open();
+                promise.then((editor) => {
                     editor.setText(requestTest);
                 });
                 return promise;
             });
-            runs(function () {
-                atom.commands.dispatch(workspaceElement, "rester:request");
+            runs(() => {
+                atom.commands.dispatch(workspaceElement, 'rester:request');
             });
-            waitsForPromise(function () {
+            waitsForPromise(() => {
                 return activationPromise;
             });
-            waitsFor(function () {
+            waitsFor(() => {
                 return atom.notifications.getNotifications().length > 0;
             });
-            runs(function () {
-                var notification = atom.notifications.getNotifications()[0];
-                expect(notification.getType()).toEqual("error");
-                expect(notification.getMessage()).toEqual("Unable to make request.");
+            runs(() => {
+                const notification = atom.notifications.getNotifications()[0];
+                expect(notification.getType()).toEqual('error');
+                expect(notification.getMessage()).toEqual('Unable to make request.');
             });
         });
 
-        it("hides modal progress view after error", function () {
-            var panel;
-            waitsForPromise(function () {
-                var promise = atom.workspace.open();
-                promise.then(function (editor) {
+        it('Hides modal progress view after error', function () {
+            waitsForPromise(() => {
+                const promise = atom.workspace.open();
+                promise.then((editor) => {
                     editor.setText(requestTest);
                 });
                 return promise;
             });
-            runs(function () {
-                expect(workspaceElement.querySelector(".rester-request-progress")).not.toExist();
-                atom.commands.dispatch(workspaceElement, "rester:request");
+            runs(() => {
+                expect(workspaceElement.querySelector('.rester-request-progress')).not.toExist();
+                atom.commands.dispatch(workspaceElement, 'rester:request');
             });
-            waitsForPromise(function () {
+            waitsForPromise(() => {
                 return activationPromise;
             });
-            waitsFor(function () {
+            waitsFor(() => {
                 return atom.notifications.getNotifications().length > 0;
             });
-            runs(function () {
-                expect(workspaceElement.querySelector(".rester-request-progress")).toExist();
+            runs(() => {
+                expect(workspaceElement.querySelector('.rester-request-progress')).toExist();
             });
-            waitsFor(function () {
-                var item = workspaceElement.querySelector(".rester-request-progress");
+            let panel;
+            waitsFor(() => {
+                const item = workspaceElement.querySelector('.rester-request-progress');
                 panel = atom.workspace.panelForItem(item);
                 return panel !== undefined;
             });
-            runs(function () {
+            runs(() => {
                 expect(panel.isVisible()).toBe(false);
             });
         });
     });
 
-    describe("Response", function () {
+    describe('Response', () => {
 
-        var responseSent,
-            server;
+        let responseSent;
+        let server;
 
-        beforeEach(function () {
+        beforeEach(() => {
             responseSent = false;
             server = http.createServer();
-            server.on("request", function(request, response) {
+            server.on('request', (request, response) => {
                 response.statusCode = 200;
-                response.write("Hello, world!");
+                response.write('Hello, world!');
                 response.end();
                 responseSent = true;
             });
             server.listen(port);
         });
-        afterEach(function () {
+        afterEach(() => {
             server.close();
         });
 
-        it("hides modal progress view after response", function () {
-            var panel;
+        it('Hides modal progress view after response', () => {
             // Open an editor and set the text.
-            waitsForPromise(function () {
-                var promise = atom.workspace.open();
-                promise.then(function (editor) {
+            waitsForPromise(() => {
+                const promise = atom.workspace.open();
+                promise.then((editor) => {
                     editor.setText(requestTest);
                 });
                 return promise;
             });
             // Run the command.
-            runs(function () {
-                expect(workspaceElement.querySelector(".rester-request-progress")).not.toExist();
-                atom.commands.dispatch(workspaceElement, "rester:request");
+            runs(() => {
+                expect(workspaceElement.querySelector('.rester-request-progress')).not.toExist();
+                atom.commands.dispatch(workspaceElement, 'rester:request');
             });
             // Wait until the server finished sending the response.
-            waitsFor(function () {
+            waitsFor(() => {
                 return responseSent;
             });
-            waitsFor(function () {
-                var item = workspaceElement.querySelector(".rester-request-progress");
+            let panel;
+            waitsFor(() => {
+                const item = workspaceElement.querySelector(".rester-request-progress");
                 panel = atom.workspace.panelForItem(item);
                 return panel !== undefined;
             });
-            runs(function () {
+            runs(() => {
                 expect(panel.isVisible()).toBe(false);
             });
         });
 
-        it("Reactivates request editor after response", function () {
-            var requestEditor, requestPane;
+        it('Reactivates request editor after response', () => {
+            let requestEditor;
+            let requestPane;
             // Open an editor and set the text.
-            waitsForPromise(function () {
-                var promise = atom.workspace.open();
-                promise.then(function (editor) {
+            waitsForPromise(() => {
+                const promise = atom.workspace.open();
+                promise.then((editor) => {
                     editor.setText(requestTest);
                     requestEditor = editor;
                     requestPane = atom.workspace.paneForItem(requestEditor);
@@ -190,89 +190,85 @@ describe("RESTer", function () {
                 return promise;
             });
             // Run the command.
-            runs(function () {
-                atom.commands.dispatch(workspaceElement, "rester:request");
+            runs(() => {
+                atom.commands.dispatch(workspaceElement, 'rester:request');
             });
             // Wait until the server finished sending the response.
-            waitsFor(function () {
+            waitsFor(() => {
                 return responseSent;
             });
             // There should be an additional pane with the response.
-            runs(function () {
-                var currentPane = atom.workspace.getActivePane(),
-                    currentEditor = atom.workspace.getActiveTextEditor();
-                expect(currentPane).toBe(requestPane);
-                expect(currentEditor).toBe(requestEditor);
+            runs(() => {
+                expect(atom.workspace.getActivePane()).toBe(requestPane);
+                expect(atom.workspace.getActiveTextEditor()).toBe(requestEditor);
             });
         });
 
-        it("Writes response to new editor", function () {
+        it('Writes response to new editor', () => {
             // Open an editor and set the text.
-            waitsForPromise(function () {
-                var promise = atom.workspace.open();
-                promise.then(function (editor) {
+            waitsForPromise(() => {
+                const promise = atom.workspace.open();
+                promise.then((editor) => {
                     editor.setText(requestTest);
                 });
                 return promise;
             });
             // Run the command.
-            runs(function () {
-                expect(workspaceElement.querySelector(".rester-request-progress")).not.toExist();
-                atom.commands.dispatch(workspaceElement, "rester:request");
+            runs(() => {
+                expect(workspaceElement.querySelector('.rester-request-progress')).not.toExist();
+                atom.commands.dispatch(workspaceElement, 'rester:request');
             });
             // Wait until the server finished sending the response.
-            waitsFor(function () {
+            waitsFor(() => {
                 return responseSent;
             });
             // There should be an additional pane with the response.
-            runs(function () {
+            runs(() => {
                 expect(atom.workspace.getPanes().length).toEqual(2);
             });
         });
 
-        it("Writes response to previously used editor", function () {
-            var requestEditor;
+        it('Writes response to previously used editor', function () {
+            let requestEditor;
             // Open an editor and set the text.
-            waitsForPromise(function () {
-                var promise = atom.workspace.open();
-                promise.then(function (editor) {
+            waitsForPromise(() => {
+                const promise = atom.workspace.open();
+                promise.then((editor) => {
                     editor.setText(requestTest);
                     requestEditor = editor;
                 });
                 return promise;
             });
             // Run the command.
-            runs(function () {
-                atom.commands.dispatch(workspaceElement, "rester:request");
+            runs(() => {
+                atom.commands.dispatch(workspaceElement, 'rester:request');
             });
             // Wait until the server finished sending the response.
-            waitsFor(function () {
+            waitsFor(() => {
                 return responseSent;
             });
             // There should be an additional pane with the response.
-            runs(function () {
+            runs(() => {
                 expect(atom.workspace.getTextEditors().length).toEqual(2);
                 responseSent = false;
-                atom.commands.dispatch(workspaceElement, "rester:request");
+                atom.commands.dispatch(workspaceElement, 'rester:request');
             });
             // Wait until the server finished sending the response.
-            waitsFor(function () {
+            waitsFor(() => {
                 return responseSent;
             });
             // There should be an additional pane with the response.
-            runs(function () {
+            runs(() => {
                 expect(atom.workspace.getTextEditors().length).toEqual(2);
                 responseSent = false;
-                atom.commands.dispatch(workspaceElement, "rester:request");
+                atom.commands.dispatch(workspaceElement, 'rester:request');
             });
         });
 
         describe("Cancel", function () {
-            it("Request can be canceled while waiting for response", function () {});
-            it("Request can be canceled while receiving response", function () {});
+            it("Request can be canceled while waiting for response");
+            it("Request can be canceled while receiving response");
         });
-
         it("Sets response editor grammar");
-
     });
 });
