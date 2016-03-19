4348a90818ccba543d9c414fb6adf3b51086c539
PJ Dietz <pjdietz@gmail.com>
2016-03-19 17:20:14 -0400

    Requests are delimited by lines containing three or more hyphens and no other characters
    Close #4

--- spec/selection-spec.js
+++ spec/selection-spec.js
@@ -11,12 +11,46 @@ This is the first line of the body.
 This is the second line of the body.
 `.trim();
 
+const request1 = `
+GET /path HTTP/1.1
+Host: localhost:8080
+`.trim();
+
+const request2 = `
+POST /path HTTP/1.1
+Host: localhost:8080
+Content-type: text/plain
+
+This is the first line of the body.
+
+--- This is just for decoration and is not a request delimieter ---
+
+This is the second line of the body.
+`.trim();
+
+const request3 = `
+DELETE /path HTTP/1.1
+Host: localhost:8080
+`.trim();
+
+const delimitedRequests = `
+${request1}
+
+---
+
+${request2}
+
+--------------------------------------------------------------------------------
+
+${request3}
+`.trim();
+
 describe('Selection', function () {
     beforeEach(function () {
         this.editor = undefined;
         waitsForPromise(() => { return atom.workspace.open(); });
     });
-    describe('When there is no selection', function () {
+    describe('When there is no selection and no delimiters', function () {
         it('Includes entire buffer', function () {
             let editor = atom.workspace.getActiveTextEditor();
             editor.setText(request);
@@ -53,5 +87,65 @@ describe('Selection', function () {
             expect(selection).not.toContain(lines[2]);
             expect(selection).toContain(lines[4]);
         });
+    });
+    describe('When there editor contains request delimiters', function () {
+        let editor;
+        let selection;
+        beforeEach(function () {
+            editor = atom.workspace.getActiveTextEditor();
+            editor.setText(delimitedRequests);
+        });
+        describe('And the cursor is located in a request that ends with a delimiter', function () {
+            beforeEach(function () {
+                editor.setCursorBufferPosition([0, 0]);
+                let selector = new Selector(editor);
+                selection = selector.getSelection();
+            });
+            it('Includes the delimited request', function () {
+                expect(selection).toContain(request1);
+            });
+            it('Does not include the other requests', function () {
+                expect(selection).not.toContain(request2);
+                expect(selection).not.toContain(request3);
+            });
+            it('Does not include delimiter', function () {
+                expect(selection).not.toMatch(/^-{3,}$/gm);
+            });
+        });
+        describe('And the cursor is located in a request that start with a delimiter', function () {
+            beforeEach(function () {
+                editor.setCursorBufferPosition([17, 0]);
+                let selector = new Selector(editor);
+                selection = selector.getSelection();
+            });
+            it('Includes the delimited request', function () {
+                expect(selection).toContain(request3);
+            });
+            it('Does not include the other requests', function () {
+                expect(selection).not.toContain(request1);
+                expect(selection).not.toContain(request2);
+            });
+            it('Does not include delimiter', function () {
+                expect(selection).not.toMatch(/^-{3,}$/gm);
+            });
+        });
+        describe('And the cursor is located between two delimiters', function () {
+            beforeEach(function () {
+                editor.setCursorBufferPosition([10, 0]);
+                let selector = new Selector(editor);
+                selection = selector.getSelection();
+            });
+            it('Includes the delimited request', function () {
+                expect(selection).toContain(request2);
+            });
+            it('Does not include the other requests', function () {
+                expect(selection).not.toContain(request1);
+                expect(selection).not.toContain(request3);
+            });
+            it('Does not include delimiter', function () {
+                expect(selection).not.toMatch(/^-{3,}$/gm);
+            });
+        });
+
     });
 });
