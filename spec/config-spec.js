846fef9ce96e60b8673d189078444b639164bfcb
PJ Dietz <pjdietz@gmail.com>
2016-03-19 21:03:45 -0400

    Optionally follows redirects
    Closes #5

--- spec/config-spec.js
+++ spec/config-spec.js
@@ -15,10 +15,30 @@ describe('Config', function () {
         // Create and start a server.
         this.server = http.createServer();
         this.server.on('request', (request, response) => {
-            response.statusCode = 200;
-            response.setHeader('Content-Type', 'text/plain');
-            response.write('Hello, world!');
-            response.end();
+            if (request.url === '/') {
+                // Hello, world!
+                response.statusCode = 200;
+                response.setHeader('Content-Type', 'text/plain');
+                response.write('Hello, world!');
+                response.end();
+            } else if (request.url.startsWith('/redirect/')) {
+               // Redirect the client from /redirect/{code}/{n} to
+               // /redirect/{code}/{n-1}; If n = 1, redirects to /hello
+               let location = '/';
+               let parts = request.url.slice(1).split('/');
+               let code = parts[1];
+               let n = parseInt(parts[2], 10);
+               if (n > 1) {
+                   location = '/redirect/' + code + '/' + (n - 1);
+               }
+               response.statusCode = code;
+               response.setHeader('Location', location);
+               response.end();
+           } else {
+               response.statusCode = 404;
+               response.write(`Not found: ${request.url}`);
+               response.end();
+           }
         });
         this.server.listen(port);
 
@@ -53,8 +73,8 @@ describe('Config', function () {
         };
     });
 
-    afterEach(function () {
-        this.server.close();
+    afterEach(function (done) {
+        this.server.close(done);
         this.server = undefined;
     });
 
@@ -117,6 +137,70 @@ describe('Config', function () {
                     this.waitsForResponse();
                 });
                 assertShowsHeaders();
+            });
+        });
+    });
+
+    describe('Redrects', function () {
+
+        function assertShowsRedirectResponse() {
+            it('Displays redirect response', function () {
+                let response = this.getResponseEditor().getText();
+                expect(response).toContain('HTTP/1.1 302 Found');
+            });
+        }
+
+        function assertShowsFinalRespose() {
+            it('Displays final response', function () {
+                let response = this.getResponseEditor().getText();
+                expect(response).toContain('HTTP/1.1 200 OK');
+            });
+        }
+
+        describe('When followRedircts is true', function () {
+            beforeEach(function () {
+                atom.config.set('rester.followRedirects', true);
+                atom.config.set('rester.redirectStatusCodes', ['302']);
+            });
+            describe('And there are no overrides', function () {
+                beforeEach(function () {
+                    this.dispatchCommand(`
+                        GET http://localhost:${port}/redirect/302/3`);
+                    this.waitsForResponse();
+                });
+                assertShowsFinalRespose();
+            });
+            describe('And request contains @followerRedirects: false', function () {
+                beforeEach(function () {
+                    this.dispatchCommand(`
+                        GET http://localhost:${port}/redirect/302/3
+                        @followRedirects: false`);
+                    this.waitsForResponse();
+                });
+                assertShowsRedirectResponse();
+            });
+        });
+        describe('When followRedircts is false', function () {
+            beforeEach(function () {
+                atom.config.set('rester.followRedirects', false);
+                atom.config.set('rester.redirectStatusCodes', ['302']);
+            });
+            describe('And there are no overrides', function () {
+                beforeEach(function () {
+                    this.dispatchCommand(`
+                        GET http://localhost:${port}/redirect/302/3`);
+                    this.waitsForResponse();
+                });
+                assertShowsRedirectResponse();
+            });
+            describe('And request contains @followerRedirects', function () {
+                beforeEach(function () {
+                    this.dispatchCommand(`
+                        GET http://localhost:${port}/redirect/302/3
+                        @followRedirects`);
+                    this.waitsForResponse();
+                });
+                assertShowsFinalRespose();
             });
         });
     });
